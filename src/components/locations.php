84692a54549a3ada24868393c319ee381dadd415
Arnab Chaudhuri <arnab15102012@gmail.com>
2022-05-23 20:59:55 +0530

    UI

--- src/components/locations.php
+++ src/components/locations.php
@@ -1,4 +1,4 @@
-<div class="locations">
+<!-- <div class="locations">
     <h2 class="section_heading">Where are we<span class="section_heading_red">located<span></h2>
     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br/>Lorem Ipsum has been the industry's standard dummy<p>
     <div class="locations_list">
@@ -63,6 +63,109 @@
             <img class="location_img" src="./assets/images/location_1.png"/>
             <h5 class="location_name">Nevada</h5>
             <img class="location_img_hover" src="./assets/images/location_1.png"/>
+        </div>
+    </div>
+</div> -->
+
+<div class="locations">
+    <h2 class="section_heading">Where are we<span class="section_heading_red">located<span></h2>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br/>Lorem Ipsum has been the industry's standard dummy<p>
+    <div class="locations_list">
+        <div class="location_card">
+            <span>Nevada</span>    
+            <img src="./assets/images/location_1.png" />
+            <div>
+                <h3>View Jobs</h3>
+            </div>
+            <a href="#"></a>
+        </div>
+        <div class="location_card">
+            <span>Nevada</span>    
+            <img src="./assets/images/location_1.png" />
+            <div>
+                <h3>View Jobs</h3>
+            </div>
+            <a href="#"></a>
+        </div>
+        <div class="location_card">
+            <span>Nevada</span>    
+            <img src="./assets/images/location_1.png" />
+            <div>
+                <h3>View Jobs</h3>
+            </div>
+            <a href="#"></a>
+        </div>
+        <div class="location_card">
+            <span>Nevada</span>    
+            <img src="./assets/images/location_1.png" />
+            <div>
+                <h3>View Jobs</h3>
+            </div>
+            <a href="#"></a>
+        </div>
+    </div>
+    <div class="locations_list_2">
+        <div class="location_card">
+            <span>Nevada</span>    
+            <img src="./assets/images/location_1.png" />
+            <div>
+                <h3>View Jobs</h3>
+            </div>
+            <a href="#"></a>
+        </div>            
+        <div class="location_search">
+            <div>Find your dream job</div>
+            <form action="./index.php">
+                <select name="jobs" id="jobs">
+                    <option value="" disabled selected>Select your location</option>
+                    <option value="saab">Alaska</option>
+                    <option value="opel">Europe</option>
+                    <option value="audi">USA</option>
+                </select>
+                <input class="btn-outline btn-blue" type="submit" value="Search">
+            </form>
+        </div>
+        <div class="location_card">
+            <span>Nevada</span>    
+            <img src="./assets/images/location_1.png" />
+            <div>
+                <h3>View Jobs</h3>
+            </div>
+            <a href="#"></a>
+        </div>
+    </div>
+    <div class="locations_list">
+        <div class="location_card">
+            <span>Nevada</span>    
+            <img src="./assets/images/location_1.png" />
+            <div>
+                <h3>View Jobs</h3>
+            </div>
+            <a href="#"></a>
+        </div>
+        <div class="location_card">
+            <span>Nevada</span>    
+            <img src="./assets/images/location_1.png" />
+            <div>
+                <h3>View Jobs</h3>
+            </div>
+            <a href="#"></a>
+        </div>
+        <div class="location_card">
+            <span>Nevada</span>    
+            <img src="./assets/images/location_1.png" />
+            <div>
+                <h3>View Jobs</h3>
+            </div>
+            <a href="#"></a>
+        </div>
+        <div class="location_card">
+            <span>Nevada</span>    
+            <img src="./assets/images/location_1.png" />
+            <div>
+                <h3>View Jobs</h3>
+            </div>
+            <a href="#"></a>
         </div>
     </div>
 </div>
